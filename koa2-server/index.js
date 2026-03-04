1a2e22b0edfaf197bef868bac2b90ed9f14f462a
Yukiookami <1073898183@qq.com>
2026-03-04 17:13:55 +0900

    配置windows上的访问端口

--- koa2-server/index.js
+++ koa2-server/index.js
@@ -2,7 +2,9 @@ import "dotenv/config";
 import app from "./src/app.js";
 
 const PORT = process.env.PORT || 3000;
+const HOST = "0.0.0.0";
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST, () => {
   console.log(`🚀 Koa2 Server running at http://localhost:${PORT}`);
+  console.log(`🌐 Accessible via Tailscale at http://shiro-windows:${PORT}`);
 });
